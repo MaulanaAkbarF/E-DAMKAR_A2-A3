553d6421239e0b51f25c397f00ea9599efadc69c
Akhdan Robbani <danakhdan12@gmail.com>
2023-08-16 00:15:57 +0700

    add watermark on laporan camera

--- lib/src/laporans/controllers/pelaporan_controller.dart
+++ lib/src/laporans/controllers/pelaporan_controller.dart
@@ -7,6 +7,8 @@ import 'package:edamkar_1/utils/app_style.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
+import 'package:intl/intl.dart';
 
 class PelaporanController extends GetxController {
   final TextEditingController namaBencanaCon = TextEditingController();
@@ -14,20 +16,31 @@ class PelaporanController extends GetxController {
   final TextEditingController deskripsiCon = TextEditingController();
   final formKey = GlobalKey<FormState>();
   var iduser;
+  var namaUser = '';
   var dataArgs = Get.arguments;
+  String alamat = '';
 
   Rx<File?> image = Rx<File?>(null);
   String? imageName;
   var imagePath;
   RxBool showSpinner = false.obs;
   final ImagePicker _picker = ImagePicker();
-  var img;
 
   @override
   void onInit() {
     super.onInit();
     DataUser().getNoHp().then((value) => noTelpCon.text = value);
     DataUser().getUserId().then((value) => iduser = value);
+    DataUser().getNama().then((value) => namaUser = value);
+    alamat = dataArgs["jalan"] +
+        ', ' +
+        dataArgs["desa"] +
+        ', ' +
+        dataArgs["kecamatan"] +
+        ', ' +
+        dataArgs["kota"] +
+        ', ' +
+        dataArgs["kodepos"];
   }
 
   void _kirimNotifikasi() async {
@@ -54,24 +67,14 @@ class PelaporanController extends GetxController {
       print('Gagal mengirim data. Kode status: ${response.statusCode}');
     }
   }
-  
+
   void pushLaporan() async {
     // _kirimNotifikasi();
     showSpinner.value = true;
     String title = "${iduser.toString()}_image_${getRandomString(30)}";
     DateTime now = DateTime.now();
     DateTime date = DateTime(now.year, now.month, now.day);
-    String alamat = dataArgs["jalan"] +
-        ', ' +
-        dataArgs["desa"] +
-        ', ' +
-        dataArgs["kecamatan"] +
-        ', ' +
-        dataArgs["kota"] +
-        ', ' +
-        dataArgs["kodepos"];
 
-    
     //eksekuis post kirim photo
     await APIClient().postMulti('addImage', image, imagePath, title);
     var result = await APIClient().postData('addPelaporan', {
@@ -81,7 +84,8 @@ class PelaporanController extends GetxController {
       'nama_hewan': '-',
       'waktu_pelaporan': '12:00',
       'tgl_pelaporan': date.toString().replaceAll("00:00:00.000", ""),
-      'urgensi': dataArgs['idKategori'] != 1 ? namaBencanaCon.text : "kebakaran",
+      'urgensi':
+          dataArgs['idKategori'] != 1 ? namaBencanaCon.text : "kebakaran",
       'alamat': alamat,
       'latitude': dataArgs["latitude"].toString(),
       'longitude': dataArgs["longitude"].toString(),
@@ -114,16 +118,36 @@ class PelaporanController extends GetxController {
   }
 
   Future getImage() async {
-    final imagePicked = await _picker.pickImage(source: ImageSource.gallery);
+    final imagePicked = await _picker.pickImage(source: ImageSource.camera);
 
     if (imagePicked != null) {
-      image.value = File(imagePicked.path);
+      image.value = await drawTextOnImage(imagePicked);
+      // image.value = File(imagePicked.path);
       imageName = imagePicked.name;
       imagePath = imagePicked.path;
       update();
     } else {
       print('no image selected');
     }
+  }
+
+  Future<File> drawTextOnImage(XFile xFile) async {
+    // var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    var decodeImg = img.decodeImage(File(xFile.path).readAsBytesSync());
+    String tdata = DateFormat("HH:mm:ss").format(DateTime.now());
+    String cdate2 = DateFormat("dd, MMMM, yyyy").format(DateTime.now());
+    String day = DateFormat("EEEEE").format(DateTime.now());
+    img.drawString(decodeImg!,
+        "Pelapor : $namaUser \n\nwaktu : $tdata \n\nhari : $day \n\ntanggal : $cdate2 \n\nalamat: $alamat",
+        font: img.arial48, x: 40);
+    // img.drawString(decodeImg!, arial24, 0, 0, DateTime.now().toString());
+
+    var encodeImage = img.encodeJpg(decodeImg, quality: 100);
+
+    var finalImage = File(xFile.path)..writeAsBytesSync(encodeImage);
+
+    return finalImage;
   }
 
   // void goToCamera() async {
