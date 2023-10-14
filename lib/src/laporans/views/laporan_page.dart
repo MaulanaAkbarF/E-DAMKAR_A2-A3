eea4c4429e6b2f497aead90e0ce1b8e6633fa8bc
Akhdanre <danakhdan12@gmail.com>
2023-10-14 14:02:31 +0700

    Adjustment on pelaporan view page

--- lib/src/laporans/views/laporan_page.dart
+++ lib/src/laporans/views/laporan_page.dart
@@ -1,5 +1,6 @@
 import 'package:edamkar_1/routes/app_pages.dart';
 import 'package:edamkar_1/utils/app_style.dart';
+import 'package:edamkar_1/utils/style_n_color.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -87,13 +88,84 @@ class _LaporanPageState extends State<LaporanPage> {
                 Align(
                   alignment: FractionalOffset.topLeft,
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 16, right: 16, top: 20),
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, top: 20),
                     child: Text(teks['Header'],
                         overflow: TextOverflow.ellipsis,
                         maxLines: 4,
                         style: teksStyle['Bold1']),
                   ),
                 ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                  top: paddingVertical1,
+                  left: paddingHorozontal1,
+                  right: paddingHorozontal1),
+              child: Container(
+                child: InkWell(
+                  onTap: () =>
+                      Get.toNamed(Routes.mpLap, arguments: "kebakaran"),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border:
+                          Border.all(color: Colors.grey.shade300, width: 1.2),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Color.fromRGBO(253, 232, 232, 1.000),
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(12),
+                              topLeft: Radius.circular(12),
+                            ),
+                          ),
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 24, bottom: 24),
+                            child: SvgPicture.asset(
+                              'semuaAset/gambar/kebakaran-icon.svg',
+                              width: 50, // ukuran ikon yang diinginkan
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: FractionalOffset.topCenter,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 16, left: 16),
+                            child: Text("Laporan Kebakaran",
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 4,
+                                style: StyleTxt.sb(size: 18)),
+                          ),
+                        ),
+                        Align(
+                          alignment: FractionalOffset.topCenter,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 8, left: 16, right: 16),
+                            child: Text("Salag satu layanan dari E-Damkar",
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 4,
+                                style: TextStyle(
+                                    fontFamily: "font/inter_bold.ttf",
+                                    color: Colors.black45,
+                                    fontSize: paddingHorozontal1 / 1.5)),
+                          ),
+                        ),
+                        SizedBox(
+                          height: paddingVertical1,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16),
@@ -103,130 +175,6 @@ class _LaporanPageState extends State<LaporanPage> {
                   mainAxisSpacing: 16,
                   crossAxisSpacing: 16,
                   children: [
-                    for (final teks in teksLaporan)
-                      for (final teksStyle in teksStyleLaporan)
-                        InkWell(
-                          onTap: () => Get.toNamed(Routes.mpLap,
-                              arguments: "bencanaalam"),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              border: Border.all(
-                                  color: Colors.grey.shade300, width: 1.2),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  decoration: const BoxDecoration(
-                                    color: Color.fromRGBO(253, 232, 232, 1.000),
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(12),
-                                      topLeft: Radius.circular(12),
-                                    ),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(top: 24, bottom: 24),
-                                    child: SvgPicture.asset(
-                                      'semuaAset/gambar/bencana-alam-icon.svg',
-                                      width: 50, // ukuran ikon yang diinginkan
-                                    ),
-                                  ),
-                                ),
-                                Align(
-                                  alignment: FractionalOffset.topLeft,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 16, left: 16),
-                                    child: Text("Laporan Bencana Alam",
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 4,
-                                        style: teksStyle['SemiBold3']),
-                                  ),
-                                ),
-                                Align(
-                                  alignment: FractionalOffset.topLeft,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 8, left: 16, right: 16),
-                                    child: Text(teks['deskripsiLaporan'],
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 4,
-                                        style: TextStyle(
-                                            fontFamily: "font/inter_bold.ttf",
-                                            color: Colors.black45,
-                                            fontSize:
-                                                paddingHorozontal1 / 1.5)),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                    for (final teks in teksLaporan)
-                      for (final teksStyle in teksStyleLaporan)
-                        InkWell(
-                          onTap: () =>
-                              Get.toNamed(Routes.mpLap, arguments: "kebakaran"),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              border: Border.all(
-                                  color: Colors.grey.shade300, width: 1.2),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  decoration: const BoxDecoration(
-                                    color: Color.fromRGBO(253, 232, 232, 1.000),
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(12),
-                                      topLeft: Radius.circular(12),
-                                    ),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(top: 24, bottom: 24),
-                                    child: SvgPicture.asset(
-                                      'semuaAset/gambar/kebakaran-icon.svg',
-                                      width: 50,
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 16, left: 16, right: 16),
-                                  child: Text(
-                                    "Laporan Kebakaran",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                    style: teksStyle['SemiBold3'],
-                                  ),
-                                ),
-                                Flexible(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 8, left: 16, right: 16),
-                                    child: Text(
-                                      teks['deskripsiLaporan'],
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,
-                                      style: TextStyle(
-                                          fontFamily: "font/inter_bold.ttf",
-                                          color: Colors.black45,
-                                          fontSize: paddingHorozontal1 / 1.5),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
                     for (final teks in teksLaporan)
                       for (final teksStyle in teksStyleLaporan)
                         InkWell(
@@ -252,8 +200,8 @@ class _LaporanPageState extends State<LaporanPage> {
                                   ),
                                   alignment: Alignment.center,
                                   child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(top: 24, bottom: 24),
+                                    padding: const EdgeInsets.only(
+                                        top: 24, bottom: 24),
                                     child: SvgPicture.asset(
                                       'semuaAset/gambar/hewan-buas-icon.svg',
                                       width: 50, // ukuran ikon yang diinginkan
@@ -263,7 +211,8 @@ class _LaporanPageState extends State<LaporanPage> {
                                 Align(
                                   alignment: FractionalOffset.topLeft,
                                   child: Padding(
-                                    padding: const EdgeInsets.only(top: 16, left: 16),
+                                    padding: const EdgeInsets.only(
+                                        top: 16, left: 16),
                                     child: Text("Laporan Hewan Buas",
                                         overflow: TextOverflow.ellipsis,
                                         maxLines: 4,
@@ -314,8 +263,8 @@ class _LaporanPageState extends State<LaporanPage> {
                                   ),
                                   alignment: Alignment.center,
                                   child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(top: 28, bottom: 28),
+                                    padding: const EdgeInsets.only(
+                                        top: 28, bottom: 28),
                                     child: SvgPicture.asset(
                                       'semuaAset/gambar/penyelamatan-icon.svg',
                                       width: 70, // ukuran ikon yang diinginkan
@@ -325,7 +274,8 @@ class _LaporanPageState extends State<LaporanPage> {
                                 Align(
                                   alignment: FractionalOffset.topLeft,
                                   child: Padding(
-                                    padding: const EdgeInsets.only(top: 16, left: 16),
+                                    padding: const EdgeInsets.only(
+                                        top: 16, left: 16),
                                     child: Text("Laporan Penyelamatan",
                                         overflow: TextOverflow.ellipsis,
                                         maxLines: 4,
@@ -372,8 +322,8 @@ class _LaporanPageState extends State<LaporanPage> {
             Align(
               alignment: Alignment.center,
               child: Padding(
-                padding:
-                    const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 24),
+                padding: const EdgeInsets.only(
+                    top: 16, left: 16, right: 16, bottom: 24),
                 child: Material(
                   clipBehavior: Clip.antiAliasWithSaveLayer,
                   borderRadius: BorderRadius.circular(8),
